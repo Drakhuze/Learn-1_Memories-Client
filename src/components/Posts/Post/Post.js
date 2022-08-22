5bc714e06166e1a9ebd2fe44ed17f46b27b050cd
Steven Setiawan Kusuma <steven.kusuma@binus.edu>
2022-08-22 08:10:59 +0700

    Initial Commit

---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material'
+import ThumbUpAltIcon from '@mui/icons'
+
+import './styles.css';
+
+const Post = () => {
+    return (
+        <h1>Post</h1>
+    );
+}
+
+export default Post;
